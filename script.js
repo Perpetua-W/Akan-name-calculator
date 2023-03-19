3c3e4157a6812337447a8ae8c3c96981d3681b13
Perpetua-W <perpetua.gatheru@gmail.com>
2023-03-19 20:58:04 +0300

    gender defined

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ myf.addEventListener('submit',(e)=>{
     const MM = Number(myf['month'].value);
     const YY = Number(myf['year'].value) % 100;
     const CC = Math.ceil(Number(myf['year'].value)/100);
-
+    const gender = myf['gender'].value;
    
 
     const day_of_the_week = Math.ceil(Math.abs(( ( (CC/4) - 2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7));
